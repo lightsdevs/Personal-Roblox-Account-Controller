8684a32f12bd823e40af483dffbc8726dc1791a4
lightsdevs <lightsdevs@gmail.com>
2024-08-07 10:37:53 -0400

    Add chromium login

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 from singleton import Singleton
 from player import Player
 from gui import render
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+
+players = []
 
 def main():
     Singleton.create_mutex("ROBLOX_singletonEvent") # create anti roblox mutex
-    me = Player("_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_what you know abotu skibidi rizz")
+    me = players[0]
     print(me.username+" "+str(me.userid))
     me.joinGame(14067600077) # join game by id
-    me.joinUser("okdokiboomer") # join user by username
+    # me.joinUser("okdokiboomer") # join user by username
     render()
 
+def addPlayer():
+    driver = webdriver.Chrome()
+    driver.get("https://roblox.com/login")
+    WebDriverWait(driver, 30).until(lambda d: d.current_url == "https://www.roblox.com/home")
+    cookies = driver.get_cookies()
+    driver.quit()
+    roblosecurity = None
+    for cookie in cookies:
+        if cookie["name"] == ".ROBLOSECURITY":
+            roblosecurity = cookie["value"]
+            break
+    if roblosecurity == None:
+        print("Failed to get .ROBLOSECURITY cookie")
+        return False
+    players.append(Player(roblosecurity))
+    return True
+
 if __name__ == "__main__":
-    main()
+    if addPlayer():
+        main()
